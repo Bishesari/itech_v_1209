3833467a81cec22f2004c430e3e2dd045128df61
Itech <Yasser.Bishesari@gmail.com>
2025-11-05 00:57:09 +0330

    Po

--- resources/views/components/my/flt_lbl.blade.php
+++ resources/views/components/my/flt_lbl.blade.php
@@ -1,11 +1,14 @@
 <!-- floating input (RTL) -->
 <div class="relative">
     <input placeholder=" "
-           class="peer w-full appearance-none bg-transparent border-2 rounded-lg border-gray-300 p-4 focus:outline-none focus:border-blue-600 transition" />
+           class=" peer w-full appearance-none border-2 rounded-lg border-gray-300 p-4
+           focus:outline-none focus:border-blue-700 transition" />
 
-    <label class="absolute right-4 -top-2.5 bg-accent-foreground px-2 text-sm transition-all duration-150
-    peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400
-    peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-600" >
-        نام و نام خانوادگی
-    </label>
+    <label class="absolute right-3 top-4 transition-all duration-150 text-gray-400 text-base px-2
+    peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-700 peer-focus:bg-accent-foreground
+    peer-[:not(:placeholder-shown)]:bg-accent-foreground
+    peer-[:not(:placeholder-shown)]:-top-2.5
+    peer-[:not(:placeholder-shown)]:right-4 peer-[:not(:placeholder-shown)]:text-sm
+
+    ">نام</label>
 </div>
