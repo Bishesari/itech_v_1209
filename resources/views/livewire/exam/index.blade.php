5235165aebdfc6c539a1f4f8d8c55c6848934996
SiteD-Server <Yasser.Bishesari@gmail.com>
2025-11-13 12:07:33 +0330

    Site D-Server

--- resources/views/livewire/exam/index.blade.php
+++ resources/views/livewire/exam/index.blade.php
@@ -9,7 +9,7 @@ new class extends Component {
 
     public function mount()
     {
-        $this->exams = Exam::all();
+        $this->exams = Exam::where('end_date', '>', now())->get();
     }
 
 }; ?>
@@ -22,22 +22,21 @@ new class extends Component {
     </div>
     <div class="flex gap-x-3 justify-center">
         @foreach($exams as $exam)
-
-            <flux:card size="sm" class="hover:bg-zinc-50 dark:hover:bg-zinc-700 w-80">
-                <flux:heading>{{$exam->standard->name_fa}}</flux:heading>
-                <flux:text class="mt-4">{{$exam->title}}</flux:text>
-                <flux:text class="mt-4 flex justify-between">
-                    <span>{{__('تعداد سوالات: ')}}</span>
-                    <span dir="ltr">{{$exam->question_count}}</span>
-                    <span>{{__('زمان آزمون: ')}}</span>
-                    <span dir="ltr">{{$exam->exam_time}}</span>
-                    <span>{{__('دقیقه')}}</span>
-                </flux:text>
-                <flux:text class="mt-4 flex justify-between"><span>{{__('فعال از: ')}}</span><span dir="ltr">{{$exam->jalali_start_date}}</span></flux:text>
-                <flux:text class="mt-4 flex justify-between"><span>{{__('تا: ')}}</span><span dir="ltr">{{$exam->jalali_end_date}}</span></flux:text>
-
-            </flux:card>
-
+            <a href="{{route('exam_start', ['exam'=>$exam])}}">
+                <flux:card size="sm" class="hover:bg-zinc-50 dark:hover:bg-zinc-700 w-80">
+                    <flux:heading>{{$exam->standard->name_fa}}</flux:heading>
+                    <flux:text class="mt-4">{{$exam->title}}</flux:text>
+                    <flux:text class="mt-4 flex justify-between">
+                        <span>{{__('تعداد سوالات: ')}}</span>
+                        <span dir="ltr">{{$exam->question_count}}</span>
+                        <span>{{__('زمان آزمون: ')}}</span>
+                        <span dir="ltr">{{$exam->exam_time}}</span>
+                        <span>{{__('دقیقه')}}</span>
+                    </flux:text>
+                    <flux:text class="mt-4 flex justify-between"><span>{{__('فعال از: ')}}</span><span dir="ltr">{{$exam->jalali_start_date}}</span></flux:text>
+                    <flux:text class="mt-4 flex justify-between"><span>{{__('تا: ')}}</span><span dir="ltr">{{$exam->jalali_end_date}}</span></flux:text>
+                </flux:card>
+            </a>
         @endforeach
     </div>
 
--- resources/views/livewire/exam/take.blade.php
+++ resources/views/livewire/exam/take.blade.php
@@ -2,35 +2,69 @@
 
 use App\Models\Exam;
 
+use App\Models\ExamAnswer;
+use App\Models\ExamUser;
+use App\Models\Option;
 use App\Models\Question;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Volt\Component;
 
 new class extends Component {
     public Exam $exam;
+    public ?ExamUser $examUser;
+
     public array $questionIds = [];
+
 
     public ?Question $currentQuestion = null;
     public $selectedOption = '';
 
     public function mount(Exam $exam): void
     {
-        $user = Auth::user();
-        $examUser = $user->exams()->where('exam_id', $exam->id)->first()->pivot;
-
-        $this->questionIds = json_decode($examUser->question_order, true);
+        $this->examUser = ExamUser::where('exam_id', $exam->id)->where('user_id', Auth::id())->first();
+        $this->questionIds = json_decode($this->examUser->question_order, true);
     }
 
     public function goToQuestion(int $index): void
     {
-        $this->selectedOption = '';
         $this->currentQuestion = Question::find($this->questionIds[$index]);
+
+        $selectedOptionId = ExamAnswer::where('exam_user_id', $this->examUser->id)
+            ->where('question_id', $this->currentQuestion->id)
+            ->value('option_id');
+
+        $this->selectedOption = $selectedOptionId;
+
+
+    }
+
+    public function submitAnswer(): void
+    {
+
+        // ذخیره پاسخ
+        ExamAnswer::updateOrCreate(
+            [
+                'exam_user_id' => $this->examUser->id,
+                'question_id' => $this->currentQuestion->id
+            ],
+            [
+                'option_id' => $this->selectedOption,
+                'is_correct' => Option::find($this->selectedOption)->is_correct,
+            ]
+        );
+
+        // پاک کردن انتخاب بعد از ثبت
+        $this->selectedOption = null;
     }
 
 
 }; ?>
 
 <section class="w-full">
+
+
+
+    <input type="text" wire:model="selectedOption">
     <div class="mb-2">
         <flux:heading size="xl" level="1">{{$exam->standard->name_fa}}</flux:heading>
         <flux:text color="blue" size="lg" class="my-2">{{$exam->title}}</flux:text>
@@ -38,14 +72,14 @@ new class extends Component {
     </div>
 
     <div class="flex flex-wrap gap-2 justify-evenly">
-            @foreach($questionIds as $i => $questionId)
-                <flux:button size="sm" variant="primary" color="cyan" class="cursor-pointer"
-                             wire:click="goToQuestion({{$i}})">
-                    {{++$i}}
-                </flux:button>
-                <button
-            @endforeach
-        </div>
+        @foreach($questionIds as $i => $questionId)
+            <flux:button size="sm" variant="primary" color="cyan" class="cursor-pointer"
+                         wire:click="goToQuestion({{$i}})">
+                {{++$i}}
+            </flux:button>
+            <button
+        @endforeach
+    </div>
 
     <div class="mt-3">
         @if($currentQuestion)
@@ -53,18 +87,15 @@ new class extends Component {
                 <flux:callout.heading>#{{$currentQuestion->id}} - {{$currentQuestion->text}}</flux:callout.heading>
             </flux:callout>
 
-            <flux:radio.group wire:model="selectedOption"  variant="cards" class="max-sm:flex-col my-2">
+            <flux:radio.group wire:model="selectedOption" variant="cards" class="max-sm:flex-col my-2">
                 @foreach($currentQuestion->options as $option)
-                    <flux:radio value="{{ $option->id }}" label="{{ $option->text }}" wire:key="option-{{ $option->id }}"/>
+                    <flux:radio value="{{ $option->id }}" label="{{ $option->text }}"
+                                wire:key="option-{{ $option->id }}" class="cursor-pointer"/>
                 @endforeach
-
             </flux:radio.group>
+            <flux:button wire:click="submitAnswer" class="cursor-pointer">ثبت پاسخ</flux:button>
         @else
             <p>تمام سوالات نمایش داده شد.</p>
         @endif
     </div>
-
-    <input wire:model="selectedOption">
-
-
 </section>
