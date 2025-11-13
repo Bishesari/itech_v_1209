f58c95617f9ec461cd3f6325e728a0c5721131fe
SiteD-Server <Yasser.Bishesari@gmail.com>
2025-11-14 03:01:09 +0330

    Site D-Server

--- database/migrations/2025_11_13_025127_create_exam_user_table.php
+++ database/migrations/2025_11_13_025127_create_exam_user_table.php
@@ -15,13 +15,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('exam_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-
             $table->dateTime('started_at')->nullable();
             $table->dateTime('finished_at')->nullable();
-
             $table->integer('score')->nullable();
-            $table->boolean('is_finished')->default(false);
-
             $table->json('question_order')->nullable(); // ترتیب سؤالات برای هر کاربر (اختیاری اما مفید)
             $table->timestamps();
 
